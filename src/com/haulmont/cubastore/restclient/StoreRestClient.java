3ec41da4e92c284bb9b903af81de6597efd5d5fd
glaschenko <a.glaschenko@gmail.com>
2018-05-04 23:32:07 +0400

    minor changes

--- src/com/haulmont/cubastore/restclient/StoreRestClient.java
+++ src/com/haulmont/cubastore/restclient/StoreRestClient.java
@@ -9,8 +9,6 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -18,10 +16,7 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
@@ -52,6 +47,7 @@ public class StoreRestClient {
      */
     private void login() throws IOException {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+//            HttpPost post = new HttpPost("https://www.cuba-platform.com/market/management/rest/v2/oauth/token");
             HttpPost post = new HttpPost("http://localhost:8080/app/rest/v2/oauth/token");
 
             // see cuba.rest.client.id and cuba.rest.client.secret application properties
